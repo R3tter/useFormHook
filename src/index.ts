68911bd8dc085b9e4e37dd6d7374bc6196bdc128
Yarik <uwebtechnology@gmail.com>
2020-04-06 02:18:03 +0300

    Add code
    Add library code base and types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,53 @@
-import { sum } from 'utils';
+import { useState } from 'react';
 
-console.log(sum(2, 3));
+import { Values, Validation, Target, Form, FromResult } from './types';
+import { validate } from 'utils';
+
+export const useForm = (initialValues: Values, validation: Validation): FromResult => {
+  const initialForm = {
+    values: { ...initialValues },
+    errors: {},
+    submited: false
+  };
+  const [form, setForm] = useState<Form>(initialForm);
+
+  const handleChange = ({ target }: { target: Target }) => {
+    const { value, name, type, checked } = target;
+    const newForm = {
+      ...form,
+      values: {
+        ...form.values,
+        [name]: type === 'checkbox' ? checked : value
+      }
+    };
+    setForm(newForm);
+  };
+
+  const handleSubmit = (callback: (data: Values) => any) => () => {
+    setForm({
+      ...form,
+      submited: true
+    });
+    validation ? validate(form, validation, setForm) && callback(form.values) : callback(form.values);
+  };
+
+  const reset = () => setForm(initialForm);
+
+  const setValue = (data: Values) => {
+    const newForm = {
+      ...form,
+      values: {
+        ...data
+      }
+    };
+    setForm(newForm);
+  };
+
+  return {
+    ...form,
+    handleChange,
+    handleSubmit,
+    reset,
+    setValue
+  };
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,44 @@
+import { Errors, Validation, Form } from '../types';
+
 export const sum = (a: number, b: number) => a + b;
+
+export const ThrowError = (param: boolean, message: string) => {
+  if (param) throw message;
+};
+
+export const validate = (form: Form, validation: Validation, setForm: (data: Form) => void): boolean => {
+  try {
+    let errors: Errors = {};
+
+    validation.forEach(({ rules, name, matchWithField }) => {
+      let isError = false;
+      const value = form.values[name];
+      const valueIsArr = Array.isArray(value);
+      if (!matchWithField) {
+        rules.forEach(({ regex, message }) => {
+          const isFunc = typeof regex === 'function';
+          ThrowError(valueIsArr && !isFunc, 'Use function to check array values');
+          if (!isError) {
+            // @ts-ignore
+            isError = isFunc ? !regex(value) : !regex.test(value);
+            errors = isError ? { ...errors, [name]: message } : errors;
+          }
+        });
+      } else {
+        errors =
+          value !== form.values[matchWithField]
+            ? { ...errors, [name]: `Should be equal to ${matchWithField}` }
+            : errors;
+      }
+    });
+
+    setForm({
+      ...form,
+      errors
+    });
+
+    return !Object.keys(errors).length;
+  } catch (e) {
+    console.log(`%c An error has occurred: ${e}`, 'color: red; font-size: 12px');
+  }
+};
