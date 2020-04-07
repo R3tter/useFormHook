2424362fc65b64adf68703c18bed59690af67fd6
Yarik <uwebtechnology@gmail.com>
2020-04-07 19:14:03 +0300

    Add tests for validate function

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,8 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.ts'],
         alias: {
-            utils: path.resolve(__dirname, 'src/utils/')
+            utils: path.resolve(__dirname, 'src/utils/'),
+            constants: path.resolve(__dirname, 'src/constants/'),
         }
     },
     output: {
